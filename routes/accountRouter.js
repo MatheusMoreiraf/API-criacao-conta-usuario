da8046deb684ca67ada677bc1c2f302a2db0f394
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2020-04-25 18:24:50 -0300

    POST account with validations

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import AccountModel from "../model/accountModel";
 import Cryptr from "cryptr";
 import jwt from "jsonwebtoken";
+import dataAccount from "../data/account.json";
 
 const accountRouter = express.Router();
 let securePass = new Cryptr("aes256");
@@ -12,45 +13,52 @@ accountRouter.route("/").get((req, resp) => {
         if (token) {
             jwt.verify(token, process.env.SECRET, function (err, decoded) {
                 if (err) {
-                    resp.statusMessage = "Unauthorized";
-                    resp.status(401).json({
-                        'codigo': '2',
-                        'mensagem': 'Token invalido, inexistente ou expirado'
-                    });
+                    status401(resp);
                 } else if (decoded) {
                     AccountModel.find({}, {
                         firstName: 1, lastName: 1, email: 1, phoneNumber: 1, dateBirth: 1, gender: 1
                     }, (err, account) => {
                         if (err) {
-                            resp.statusMessage = "Bad request";
-                            resp.status(400).json({
-                                'codigo': '3',
-                                'mensagem': 'Dados request enviados incorretos'
-                            });
+                            status400(resp,err);
                         } else {
-                            resp.statusMessage = "OK";
-                            resp.status(200).json(account);
+                            status200(resp,account);
                         }
                     });
                 }
             })
         } else {
-            resp.statusMessage = "Unauthorized";
-            resp.status(401).json({
-                'codigo': '2',
-                'mensagem': 'Token invalido, inexistente ou expirado'
-            });
+            status401(resp);
         }
     } catch (error) {
-        console.error(error);
-        resp.statusMessage = "Internal error";
-        resp.status(500).json({
-            'codigo': '1',
-            'mensagem': 'Erro no servidor'
-        });
+        status500(resp, error);
     }
 }).post((req, resp) => {
-
+    try {
+        let token = req.headers['token'];
+        if (token) {
+            jwt.verify(token, process.env.SECRET, function (err, decoded) {
+                if (err) {
+                    status401(resp);
+                } else if (decoded) {
+                    req.body.password = securePass.encrypt(req.body.password);
+                    let account = new AccountModel(req.body);
+                    account.save(err => {
+                        if (err) {
+                            status400(resp,err);
+                        } else {
+                            let accountAux = account.toObject();
+                            delete accountAux["password"];
+                            status201(resp, accountAux);
+                        }
+                    });
+                }
+            })
+        } else {
+            status401(resp);
+        }
+    } catch (error) {
+        status500(resp, error);
+    }
 });
 
 accountRouter.use("/:id", (req, resp, next) => {
@@ -63,4 +71,41 @@ accountRouter.route("/:id").get((req, resp) => {
 }).delete((req, resp) => {
 
 });
+
+function status200(resp,data) {
+    resp.statusMessage = "OK";
+    resp.status(200).json(data);
+}
+
+function status201(resp,data) {
+    resp.statusMessage = "Criado";
+    resp.status(201).json(data);
+}
+
+function status400(resp, err) {
+    console.error(`Erro ao salvar: ${err}`);
+    resp.statusMessage = "Bad request";
+    resp.status(400).json({
+        'codigo': '3',
+        'mensagem': 'Dados request enviados incorretos'
+    });
+}
+
+function status401(resp) {
+    resp.statusMessage = "Unauthorized";
+    resp.status(401).json({
+        'codigo': '2',
+        'mensagem': 'Token invalido, inexistente ou expirado'
+    });
+}
+
+function status500(resp, error) {
+    console.error(error);
+    resp.statusMessage = "Internal error";
+    resp.status(500).json({
+        'codigo': '1',
+        'mensagem': 'Erro no servidor'
+    });
+}
+
 export default accountRouter;
